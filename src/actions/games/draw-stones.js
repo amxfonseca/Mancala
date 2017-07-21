3bc634cbceca2ce5d6efa500b542b727ffb0c73d
elizabethprins <elizabethprins@gmail.com>
2017-07-21 16:17:12 +0200

    get draw-action to work

--- src/actions/games/draw-stones.js
+++ src/actions/games/draw-stones.js
@@ -8,16 +8,16 @@ import {
 const api = new API()
 export const DRAWN_STONES = 'DRAWN_STONES'
 
-export default (gameId, pitIndex) => {
+export default (gameId, pit) => {
   return (dispatch) => {
     const backend = api.service('games')
     api.app.authenticate()
       .then(() => {
-        backend.patch(gameId, { draw: pitIndex })
+        backend.patch(gameId, { draw: pit })
         .then(() => {
           dispatch({
             type: DRAWN_STONES,
-            payload: pitIndex
+            payload: pit
           })
         })
         .catch((error) => {
--- src/games/Pit.js
+++ src/games/Pit.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import drawStones from '../actions/games/draw-stones'
 import './Pit.css'
 
 class Pit extends PureComponent {
@@ -13,18 +14,24 @@ class Pit extends PureComponent {
 
   componentWillReceiveProps(nextProps) {}
 
-
+  draw() {
+    const { value, onDraw, currentGame, index, belongsToOwner }  = this.props
+    const pit = { index, belongsToOwner, value }
+    console.log("pit:",pit)
+    this.props.drawStones(currentGame, pit)
+  }
 
   render() {
     const { value }  = this.props
-    console.log("value", value)
 
     return (
-      <div className="pit">
-          <p>{value}</p>
+      <div className="pit" onClick={this.draw.bind(this)}>
+        <p>{value}</p>
       </div>
     )
   }
 }
 
-export default Pit
+const mapStateToProps = ({ currentGame }) => ({ currentGame })
+
+export default connect(mapStateToProps, { drawStones })(Pit)
